d5d39425180ca66ca85205c24c3617faef3f3b66
DejanTeofilovic <dejanteofilovic2@gmail.com>
2023-10-25 14:12:34 -0700

    This is the seventh commit for heroku

--- resources/js/plugins/axios.js
+++ resources/js/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  baseURL: 'http://127.0.0.1:80/api/',
+  baseURL: 'https://customgpt-dejan-d245a19da20e.herokuapp.com/api/',
 
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
