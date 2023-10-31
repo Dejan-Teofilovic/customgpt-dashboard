a6904088fd056b5ed8ff75dcfe271c9400ab91f9
Dejan Teofilovic <dejanteofilovic2@gmail.com>
2023-10-31 11:46:54 -0700

    yolo

--- heroku-queue.bat
+++ heroku-queue.bat
@@ -1,10 +1,21 @@
 @echo off
 cd "E:\UpworkProjects\dashboard\dashboard\vuexy-admin-v9.4.1\vue-laravel-version\vuexy-vuetify-vue3-laravel\javascript-version\starter-kit"
-start cmd /k heroku run php artisan queue:work --daemon
-start cmd /k heroku run php artisan queue:work --daemon
-start cmd /k heroku run php artisan queue:work --daemon
-start cmd /k heroku run php artisan queue:work --daemon
-start cmd /k heroku run php artisan queue:work --daemon
-start cmd /k heroku run php artisan queue:work --daemon
-start cmd /k heroku run php artisan queue:work --daemon
-start cmd /k heroku run php artisan queue:work --daemon
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
+start cmd /k heroku run php artisan queue:work
